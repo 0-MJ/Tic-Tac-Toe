7cc5c4f4e090810ce05b448daa7a149e1be9167c
Mervyn <102972493+0-MJ@users.noreply.github.com>
2023-02-14 15:30:20 +0530

    a rough pseudocode of the project.

---
+++ script.js
@@ -0,0 +1,29 @@
+//Gameboard object//
+
+
+//Gameboard//
+
+//Player object//
+
+
+//An Object to control the flow of the game//
+
+//User Interface//
+
+//a Js function that will render the contents of the game board array to thewebpage//
+
+
+//Functions that will allow players to add marks to a specfic spot on the board//
+
+
+
+//Logic that checks for when the game is over//
+
+
+//Button to Start/Restart the game 
+
+
+//Add a display element that congratulates the winning player!//
+
+
+//Optional use minimax to create an AI player//
