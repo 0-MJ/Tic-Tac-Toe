d6e7f8841708ab8b6ff0f70d441de715407334d8
Mervyn <102972493+0-MJ@users.noreply.github.com>
2023-02-14 20:56:13 +0530

    recreated the pseudocode Added game board module, player factory function, and game flow module
    Added two modules and a factory function to the codebase.
    - A game board module that allows for setting spots on the board, checking if the game is over, and resetting the board.
    - A player factory function that creates players with a name and a mark.
    - A game flow module that controls the flow of the game, including switching players and handling the end of the game.

--- script.js
+++ script.js
@@ -1,29 +1,63 @@
-//Gameboard object//
+//Define a Module for Gameboard//
+const GameBoard = (function() {
+  // Private array to hold the game board//
+  let board = ["", "", "", "", "", "", "", "", ""];
+ 
+ 
+// Public function to set the value of a spot on the board
+const setSpot = function ( ) {
+    
+  }
+   // Public function to check if the game is over
+   const isGameOver = function ()  {
+    // Code to check for 3-in-a-row and a tie
+  }
+ // Public function to reset the board
+    const reset = function() {
+    board = ["", "", "", "", "", "", "", "", ""];
+  }
+  
+  
+// Return all the public functions
+return {setSpot, isGameOver, reset };
+})();
+// Define factory function for players//
+const player = function(name,mark) {
+// Private variable to hold the player's name
+  let playerName = name;
+// Public function to get the player's name
+const getName = function(name){
+    return playerName;
+  }
+// Public function to get the player's mark
+  const getMark = function (mark) {
+    return mark;
+  }
+
+ // Return public functions
+ return { getName, getMark };
+};
 
 
-//Gameboard//
-
-//Player object//
-
-
-//An Object to control the flow of the game//
-
-//User Interface//
-
-//a Js function that will render the contents of the game board array to thewebpage//
-
-
-//Functions that will allow players to add marks to a specfic spot on the board//
-
-
-
-//Logic that checks for when the game is over//
-
-
-//Button to Start/Restart the game 
-
-
-//Add a display element that congratulates the winning player!//
-
+//A module  to control the flow of the game//
+const gameFlow = function()({
+     // Private variable to hold the current player
+        let currentPlayer;
+    // Private function to switch the current player
+    const switchPlayer = function () {
+        
+    }
+    // Private function to handle the end of the game
+    const handleGameOver = function () {
+    
+    }
+    // Public function to start the game
+    const startGame = function () {
+  
+    }
+    
+    // Return public function
+    return {startGame};
+})();
 
 //Optional use minimax to create an AI player//
