177e2793e3c62970ae46d7d7d353471cedb9788b
Mervyn <mervynjeremiah02@gmail.com>
2024-01-05 00:11:57 +0530

    Update script.js
    redoing the whole app completed the player object and the gameboard iife

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-undef */
 // Define a Module for Gameboardoard //
-
+/*
 const gameBoard = (function () {
-  // Private array to hold the game board//
-  let board = ['', '', '', '', '', '', '', '', ''];
+  // Array to hold the game board//
+  let board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+  
   let winningSymbol;
-  // Puboardlic function to set the value of a spot on the boardoard //
+  
+  // function to set the value of a spot on the boardoardzz //
   function setSpot(index, mark) {
-    if (board[index]) {
+    if (board[index] !== ' ') {
       console.log(`spot is already marked`);
     } else {
       board[index] = mark;
-      console.log(`Board state: ${board}`);
+      console.log(board);
       return mark;
     }
   }
 
-  // Pulic function to check if the game is over//
+  // function to check if the game is over//
   const checkWinner = function () {
     const winningCombination = [
       [0, 1, 2],
@@ -40,112 +42,51 @@ const gameBoard = (function () {
     }
     return winningSymbol;
   };
-  // Puboardlic function to reset the boardoard
+  
+  // Function to reset the boardoard
   const reset = function () {
     board = ['', '', '', '', '', '', '', '', ''];
   };
 
-  function renderBoard() {
-    const cells = document.getElementsByClassName('cell');
-    for (let i = 0; i < cells.length; i++) {
-      cells[i].textContent = board[i];
-    }
-  }
   // Return all the puboardlic functions
-  return { board, setSpot, checkWinner, reset, renderBoard, winningSymbol };
+  return { board, setSpot, checkWinner, reset, winningSymbol };
 })();
 
-// Define factory function for players//
-const player = (function () {
-  let player1;
-  let player2;
-  function playerMark(mark) {
-    if (!this.player1 && !this.player2) {
-      this.player1 = mark;
-    } else if (this.player1 && !this.player2) {
-      this.player2 = mark;
-    }
+console.log(gameBoard.setSpot(5,'o'));
+*/
+
+// Player object to store player mark //
+let playerObject = {
+  player1: '',
+  player2: '',
+
+  setPlayers: function(mark1, mark2) {
+      this.player1 = mark1;
+      this.player2 = mark2;
   }
-  return { player1, player2, playerMark };
-})();
+};
+
+// Temporary code //
+// Prompt the user for player marks
+var player1Mark = prompt("Enter mark for Player 1:");
+var player2Mark = prompt("Enter mark for Player 2:");
+
+// Set players in the game object
+game.setPlayers(player1Mark, player2Mark);
+
+// Access player variables globally
+console.log("Player 1 mark:", game.player1);
+console.log("Player 2 mark:", game.player2);
+
 
 // A module  to control the flow of the game //
-const gameFlow = (function () {
-  // Private variaboardle to hold the current player
-
-  let currentPlayer = player.player1;
-  // Private function to switch the current player
-  const switchPlayer = function () {
-    currentPlayer =
-      currentPlayer === player.player1 ? player.player2 : player.player1;
-    /* console.log(`player1${player.player1}`);
-    console.log(`player2${player.player2}`);
-    console.log(`currentPLayer${currentPlayer}`); */
-    return currentPlayer;
-  };
-  const handleGameOver = function () {
-    if (gameBoard.winningSymbol === player.player1) {
-      console.log('player1 won');
-    } else if (gameBoard.winningSymbol === player.player2) {
-      console.log('player2 won');
-    }
-  };
-
-  const runGame = function () {
-    // Public function to start the game //
-
-    gameBoard.renderBoard();
-    gameBoard.checkWinner();
-    handleGameOver();
-  };
-
-  // Return puboardlic function
-  return { switchPlayer, runGame };
+const Game = (function () {
+ 
 })();
 
+/*
+// Display module
 const displayController = (function () {
-  // Get all elements with class name "cell" //
-  const cells = document.getElementsByClassName('cell');
-  // Get all buttons //
-  const buttons = document.querySelectorAll('#x, #o, #reset');
-  // Loop through the buttons and add click event listeners //
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', () => {
-      if (buttons[i].id === 'x') {
-        // Calling the player function //
-        player.playerMark('x');
-        buttons[i].classList.add('button-clicked');
-        // Do something for X button click
-      } else if (buttons[i].id === 'o') {
-        player.playerMark('o');
-        buttons[i].classList.add('button-clicked');
-        // Do something for O button click
-      } else if (buttons[i].id === 'reset') {
-        console.log('Reset is Tapped');
-        // Remove the 'button-clicked' class from the 'x' and 'o' buttons
-        document.getElementById('x').classList.remove('button-clicked');
-        document.getElementById('o').classList.remove('button-clicked');
-        // Do something for Reset button click
-        player.player1 = null;
-        player.player2 = null;
-        gameBoard.reset();
-        gameBoard.renderBoard();
-        gameBoard.winningSymbol = null;
-      }
-    });
-  }
-  // loop through the cells and display the chosen symbol //
 
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', () => {
-      // Do something for cell click
-      if (player.player1 && player.player2&& !gameBoard.winningSymbol) {
-      const mark = gameFlow.switchPlayer();
-          gameBoard.setSpot(i, mark);
-          gameFlow.runGame();
-      }
-    });
-  }
 })();
-
-// Optional use minimax to create an AI player // */
+*/
